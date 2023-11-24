e96a522162a7994dac7045bff8807cf626048ab4
cballevre <celestin.ballevre@protonmail.com>
2023-11-24 09:53:51 +0100

    feat: Generate a build folder with public

--- src/targets/browser/index.js
+++ src/targets/browser/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Index = () => {
-  return <div>Hello React!</div>;
+  return <div>This is a private context !</div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,45 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin')
 
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
-});
-
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+function getConfig(targetName) {
+  const config = {
+    entry: `./src/targets/${targetName}/index.js`,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
         }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: `./src/targets/${targetName}/index.html`,
+        filename: "./index.html"
+      })
+    ],
+    watch: true,
+    devtool: 'source-map',
+    output: {
+      filename: `main.js`,
+      path: __dirname + `/build/${targetName === 'public' ? 'public' : ''}`
+    }
+  }
+
+  /* if(targetName === 'browser') {
+    config.plugins.push( new CopyPlugin(
+      {
+        patterns: [
+          { from: './src/manifest.webapp', to: __dirname + '/build/' }
+        ],
       }
-    ]
-  },
-  plugins: [htmlPlugin],
-  watch: true,
-  devtool: 'source-map'
-};
+    ))
+  } */
+
+  return config
+}
+
+module.exports = [getConfig('browser'), getConfig('public')];
