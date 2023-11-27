18e1182602cbd56b56fb1fb29aa472597a6d9880
cballevre <celestin.ballevre@protonmail.com>
2023-11-27 08:33:52 +0100

    feat: Add shared component

---
+++ src/components/SharedComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const SharedComponent = () => {
+  return <div>This is a component shared with public and private !</div>;
+};
+
+export { SharedComponent }
--- src/targets/public/index.js
+++ src/targets/public/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { SharedComponent } from "../../components/SharedComponent";
+
 const Index = () => {
-  return <div>This is a public context !</div>;
+  return <div>This is a public context !
+    <SharedComponent />
+  </div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById("app"));
